59ed2f6db801b4e80fc9f5f3c3d5bdbd0a3daaff
KimYoon Rae <YoonKim@KimYoonui-MacBook-Pro.local>
2016-10-05 16:46:16 -0400

    created an about page

--- web.py
+++ web.py
@@ -5,5 +5,10 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+
+@app.route('/about')
+def about():
+    return render_template("sample_page.html")
+
 app.run(debug=True) #what is it? debug?
 
